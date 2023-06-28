12589d7317cb3f0e31da45cf077a74b4d61b7577
Austin Fanger <fanger_austin@yahoo.com>
2023-06-28 18:39:45 -0500

    added inquirer prompts and connection code

---
+++ connection.js
@@ -0,0 +1,12 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost', 
+    user: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'employee_tracker'
+  });
+  
+  module.exports = connection;
